e6595b96dacf917b01e3833ed60cf90ba10514fc
Pradyumna <109955188+Pradyumna1218@users.noreply.github.com>
2026-08-31 07:24:33 +0545

    Assignment 1 SauceDEMO

---
+++ tests/first_test.py
@@ -0,0 +1,28 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+def test_get_google(driver):
+    driver.get('https://google.com')
+    time.sleep(5)
+
+def test_sauce_login(driver):
+    driver.get('https://www.saucedemo.com/')
+
+    username = driver.find_element(By.ID, 'user-name')
+    username.send_keys('standard_user')
+
+    password = driver.find_element(By.ID, 'password')
+    password.send_keys('secret_sauce')
+
+    login_button = driver.find_element(By.ID, 'login-button')
+    login_button.click()
+
+    assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+
+def test_sauce_info(driver):
+    driver.get('https://www.saucedemo.com/')
+    credentials = driver.find_element(By.CLASS_NAME, 'login_credentials_wrap-inner')
+    text = credentials.text
+    print(text)
